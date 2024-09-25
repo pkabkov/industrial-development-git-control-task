af949d8f7be00abb32808e8daa7943ff4fca7b5b
pkabkov <petyakabkov@gmail.com>
2024-09-25 21:45:26 +0300

    Добавлен переход на новую строку

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world\n");
 		System.out.println("New Feature!");
 		return;
 	}
